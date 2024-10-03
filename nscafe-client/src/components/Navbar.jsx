9b0933578e478743a01e54edfe6b51e5b1ec7a7b
Kavishka Sulochana <kavishkaigalagama@gmail.com>
2024-10-04 00:31:53 +0530

    hm-03

--- nscafe-client/src/components/Navbar.jsx
+++ nscafe-client/src/components/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '/logo.png'
 import { IoCallSharp } from "react-icons/io5";
 
 const Navbar = () => {
+    const [isSticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 0) {
+                setSticky(true)
+            } else {
+                setSticky(false)
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        };
+
+    }, []);
     const navItems =
         <>
             <li><a href='/'>Home</a></li>
@@ -29,8 +47,8 @@ const Navbar = () => {
             <li><a>Offers</a></li>
         </>
     return (
-        <header className="max-w-screen-2xl container mx-auto">
-            <div className="navbar xl:px-24">
+        <header className="max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out">
+            <div className={`navbar xl:px-24 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" : ""}`}>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
---
+++ nscafe-client/src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const SpecialDishes = () => {
+
+    const [recipes, setRecipes] = useState([]);
+    const slider = React.useRef(null);
+
+    useState(() => {
+        fetch('/menu.json')
+            .then((res) => res.json())
+            .then((data) => {
+            const special = data.filter((item) => item.category === "popular");
+            //console.log(special);
+            setRecipes(special);
+        });
+    }, []);
+
+    //settings for the slider
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (
+        <div className='section-container my-20'>
+            <div className='text-left'>
+                <p className='subtitle'>Special Dishes</p>
+                <h2 className='title md:w-[420px]'>Standout Dishes From Our Menu</h2>
+            </div>
+
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+                <div>
+                    <h3>7</h3>
+                </div>
+                <div>
+                    <h3>8</h3>
+                </div>
+            </Slider>
+        </div>
+    )
+}
+
+export default SpecialDishes
